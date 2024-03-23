65738754fed1c5077c1f2eb798b61a082ec3221b
Khalid Alhazmi <Alhazmi@outlook.com>
2024-03-23 23:40:52 +0300

    add file  helper

---
+++ file_helpers/file_helpers.go
@@ -0,0 +1,21 @@
+package file_helpers
+
+import (
+	"io"
+	"os"
+)
+
+func ReadFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
+}
